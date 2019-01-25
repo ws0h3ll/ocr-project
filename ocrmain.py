7bd70e802ffce4dbcc5674c21e93ebb3181fce35
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-01-25 10:19:42 +0000

    some commenting added

--- ocrmain.py
+++ ocrmain.py
@@ -1,17 +1,19 @@
-from pytesseract import image_to_string
+#modules
+from pytesseract import image_to_string 
 from PIL import Image
 from simpleocrscan import simplescanmulti, simplescansing
 from google import gsrch, gsrchocr
+# start of code
 print("########MENU########")
 print("Simple scan (ss)")
 print("Find a webpage (gs)")
 print("########MENU########")
-wttd = raw_input("What would you like to do ? ")
+wttd = raw_input("What would you like to do ? ") # add input sanitisation here !!!!
 if str(wttd) == "ss":
-    wttds = raw_input("Single (s) or multiple images (ms) : ")
-    if wttds == "s":
+    wttds = raw_input("Single (s) or multiple images (ms) : ") # two diff functions to either recurse through files or just do one file on its own
+    if wttds == "s": # single image
         simplescansing()
-    if wttds == "ms":
+    if wttds == "ms": # multiple images
         simplescanmulti()
 elif str(wttd) == "gs":
-    gsrchocr()
+    gsrchocr() # google search module (imported from local file)
