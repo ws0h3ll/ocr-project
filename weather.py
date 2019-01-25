582e257d1634fa5a8a286b2ec927b3d83b9836a3
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-01-25 12:41:30 +0000

    fixed weather

--- weather.py
+++ weather.py
@@ -1,16 +1,6 @@
-from darksky import forecast
-from datetime import date, timedelta
-
-BOSTON = 42.3601, 71.0589
-
-weekday = date.today()
-with forecast('', *BOSTON) as boston:
-    print(boston.daily.summary, end=='\n---\n')
-    for day in boston.daily:
-        day = dict(day = date.strftime(weekday, '%a'),
-                   sum = day.summary,
-                   tempMin = day.temperatureMin,
-                   tempMax = day.temperatureMax
-                   )
-        print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
-        weekday += timedelta(days=1)
+import urllib, json, ast
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&APPID=mykey'
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+a=ast.literal_eval(json.dumps(data)) 
+print(a)
