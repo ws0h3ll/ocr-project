1b8292478dba8a53a9024cd1ef4bd5fb164ceb53
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-02-06 12:50:59 +0000

    correct besturl to a function
    still does not work, unicode error now

--- music.py
+++ music.py
@@ -1,13 +1,11 @@
-import pafy
-import vlc
-
-url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
-video = pafy.new(url)
+import pafy, vlc
+url = str(raw_input())
+video = pafy.new(str(url))
 best = video.getbest()
-playurl = best.url
-Inst = vlc.Instance()
-player = Inst.media_player_new()
-Media = Inst.media_new(playurl)
+playurl = best.url()
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
 Media.get_mrl()
 player.set_media(Media)
 player.play()
