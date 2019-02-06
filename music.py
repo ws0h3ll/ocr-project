eb949498e11ddb18bb6c6978e5350b215d824cba
ws0h3ll <45339471+ws0h3ll@users.noreply.github.com>
2019-02-06 20:11:40 +0000

    works now whoop whoop

--- music.py
+++ music.py
@@ -1,11 +1,13 @@
-import pafy, vlc
-url = str(raw_input())
+import pafy
+import vlc
+
+url = raw_input('Enter video id ')
 video = pafy.new(url)
 best = video.getbest()
-playurl = best.url()
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-Media = Instance.media_new(playurl)
+playurl = best.url
+Inst = vlc.Instance()
+player = Inst.media_player_new()
+Media = Inst.media_new(playurl)
 Media.get_mrl()
 player.set_media(Media)
 player.play()
